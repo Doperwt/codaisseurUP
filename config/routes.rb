1fa4690def49af117da16794985396f4284018c6
Robert den Ouden <r.f.denouden@gmail.com>
2018-01-15 17:35:40 +0100

    never commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'pages/home'
 
   root to: 'pages#home'
   devise_for :users
+  get "about" => "pages#about"
+  get "contact" => "pages#contact"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
