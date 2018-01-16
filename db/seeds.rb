fdb35324887b4337ecdd60d8bf6f1269b72a6d63
Robert den Ouden <r.f.denouden@gmail.com>
2018-01-16 15:37:04 +0100

    event edit and new

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,16 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 User.destroy_all
-user1 = User.create!()
+user1 = User.create!(email:"test1@test.com",name:"test1",password:"test01")
+user2 = User.create!(email:"test2@test.com",name:"test2",password:"test02")
+user3 = User.create!(email:"test3@test.com",name:"test3",password:"test03")
 
 
-Event.create!( [ {name:"Pleinfeest",location:"Plein",size:20,price:15.5,image_url:"https://i.ytimg.com/vi/TR6EpCaF1wg/maxresdefault.jpg",description:"feest op het plein",user:users.first},
-  {name:"Straatfeest",location:"Straat",size:10,price:10.5,image_url:"http://www.statenkwartier.net/wp-content/uploads/2012/08/Straatfeest-Van-der-Heimstraat1.jpg",description:"feest op de straat",user:users.second},
-  {name:"Kroegfeest",location:"Kroeg",size:15,price:5.5,image_url:"http://rever.nl/wp-content/uploads/2014/08/Bruine-kroeg-uitgelicht.jpg",description:"feest in de kroeg",user:users.third},
+date1 = Time.now
+date2 = 2.days.from_now
+
+Event.create!( [ {name:"Pleinfeest",location:"Plein",size:20,price:15.5,image_url:"https://i.ytimg.com/vi/TR6EpCaF1wg/maxresdefault.jpg",description:"feest op het plein",user:users.first,starts_at:date1,ends_at:date2},
+  {name:"Straatfeest",location:"Straat",size:10,price:10.5,image_url:"http://www.statenkwartier.net/wp-content/uploads/2012/08/Straatfeest-Van-der-Heimstraat1.jpg",description:"feest op de straat",user:users.second,starts_at:date1,ends_at:date2},
+  {name:"Kroegfeest",location:"Kroeg",size:15,price:5.5,image_url:"http://rever.nl/wp-content/uploads/2014/08/Bruine-kroeg-uitgelicht.jpg",description:"feest in de kroeg",user:users.third,starts_at:date1,ends_at:date2},
 ]
 )
