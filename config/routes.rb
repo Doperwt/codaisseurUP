316478a1f15e81c10f98cefb70f0c483e44cd4a4
Robert den Ouden <r.f.denouden@gmail.com>
2018-01-18 15:55:42 +0100

    fixed some stuff added photo controler

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,6 @@ Rails.application.routes.draw do
   get "contact" => "pages#contact"
   get "user" => "users#show"
   get "search" => "pages#index"
-  
+  delete "photo/destroy" => "photo#destroy"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
