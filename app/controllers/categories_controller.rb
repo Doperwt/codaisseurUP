bf3b6483caa622e92a4073627944d7a820409f87
Robert den Ouden <r.f.denouden@gmail.com>
2018-01-18 17:04:53 +0100

    category pages

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,12 +1,39 @@
 class CategoriesController < ApplicationController
   before_action :set_category, only: [:show, :edit, :update]
   def index
-    @categories = Catagory.all
+    @categories = Category.all
   end
   def show ;  end
+  def new
+    @category = Category.new
+  end
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to @category, notice: "Category created"
+    else
+      render :new
+    end
+  end
+
   def edit ;  end
+  def update
+    if @category.update(category_params)
+
+      redirect_to @category, notice: "category updated"
+    else
+      render :edit
+    end
+  end
   private
   def set_category
     @category = Category.find(params[:id])
+  end
+  def category_params
+    params
+    .require(:category)
+    .permit(
+      :name, :description)
   end
 end
