d2feb9cd2f47a9d48d1ca9121b331cc382be73b8
Robert den Ouden <r.f.denouden@gmail.com>
2018-01-18 16:37:36 +0100

    photo stuff working

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,12 @@
 class CategoriesController < ApplicationController
-
-  def show
-    @themes = @room.themes
+  before_action :set_category, only: [:show, :edit, :update]
+  def index
+    @categories = Catagory.all
+  end
+  def show ;  end
+  def edit ;  end
+  private
+  def set_category
+    @category = Category.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,10 @@ Rails.application.routes.draw do
   resources :events,   except: [:destroy]
   resources :categories
   resources :profiles
+  resources :photos, only: [:destroy]
   get "about" => "pages#about"
   get "contact" => "pages#contact"
   get "user" => "users#show"
   get "search" => "pages#index"
-  delete "photo/destroy" => "photo#destroy"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
